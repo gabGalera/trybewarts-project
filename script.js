91ad02222ce23c83551d12548328ef5fa9926bd3
Gabriel Galera <gabgalera@hotmail.com>
2022-08-11 18:20:18 -0300

    Requisito 13

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validateForm() {
   const button = document.getElementById('login-btn');
   button.addEventListener('click', () => {
-    const email = document.getElementById('input-email').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('input-password').value;
     if (email === 'tryber@teste.com' && password === '123456') {
       alert('Olá, Tryber!');
