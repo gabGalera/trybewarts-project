4de05265d2afefc632b82c5310fced525a281f32
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-11 21:00:18 -0300

    Resolvido requisito 22 do projeto

--- script.js
+++ script.js
@@ -29,3 +29,50 @@ function contador() {
 }
 
 document.getElementById('textarea').addEventListener('keyup', contador);
+
+function formInfo() {
+  const form = document.querySelector('#evaluation-form');
+  const name = document.getElementById('input-name').value;
+  const lastName = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const casa = document.getElementById('house').value;
+  const familia = form.family.value;
+  console.log(familia);
+  const avaliacao = form.rate.value;
+  const observacoes = document.querySelector('#textarea').value;
+  let listMaterias = '';
+  const materias = document.querySelectorAll('.subject'); /// //
+  materias.forEach((element) => {
+    if (element.checked) { listMaterias = `${listMaterias} ${element.value},`; }
+  });
+  const list = [`${name} ${lastName}`, email, casa, familia, listMaterias, avaliacao, observacoes];
+  return list;
+}
+function removeForm() {
+  const form = document.getElementById('evaluation-form');
+  form.innerHTML = '';
+}
+
+function displayInfo() {
+  const main = document.getElementById('form-data');
+  const form = document.getElementById('evaluation-form');
+  form.style.display = 'none';
+  main.classList.add('formText');
+  const lista = formInfo();
+  const info = ['Nome: ', 'Email: ', 'Casa: ', 'Família: ', 'Matérias: ', 'Avaliação: ',
+    'Observações: '];
+  for (let index = 0; index < lista.length; index += 1) {
+    const element = document.createElement('p');
+    const aux = info[index] + lista[index];
+    console.log(aux);
+    element.innerText = info[index] + lista[index];
+    main.appendChild(element);
+  }
+}
+
+const buttonSubmit = document.getElementById('submit-btn');
+buttonSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  displayInfo();
+  removeForm();
+});
